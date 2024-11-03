a885ee5f16ce476064ba6046cf7036cf8cee1492
RAUL RAMIREZ ABRIL <91550811+rraamirez@users.noreply.github.com>
2024-11-03 18:40:19 +0100

    working o 3.2

--- routes/router_usuarios.js
+++ routes/router_usuarios.js
@@ -1,23 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken"; 
-import Usuarios from "../model/usuarios.js"; // Asegúrate de importar el modelo correctamente
+import Usuarios from "../model/usuarios.js"; 
 const router = express.Router();
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-// Asegúrate de que esta función sea asíncrona
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-
-  // Aquí llamamos a la función de verificación
-  const usuario = await verificarUsuario(username, password); // Esto es válido ya que estamos en una función async
+  const usuario = await verificarUsuario(username, password);
 
   if (usuario) {
-    req.session.usuario = usuario; 
+    req.session.usuario = usuario;
 
-    const token = jwt.sign({ usuario: usuario.username }, process.env.SECRET_KEY);
+    // con el campo admin ya puedo saber si es admin en cualquier parte de la aplicación
+    const token = jwt.sign(
+      { usuario: usuario.username, admin: usuario.admin }, 
+      process.env.SECRET_KEY
+    );
 
     res.cookie("access_token", token, {
       httpOnly: true,
@@ -28,15 +29,14 @@ router.post("/login", async (req, res) => {
   }
 });
 
-// La función verificarUsuario debería seguir siendo asíncrona
 async function verificarUsuario(username, password) {
   console.log("verificarUsuario", username, password);
   const usuario = await Usuarios.findOne({ username: username, password: password });
-  return usuario ? { username: usuario.username } : null; 
+  return usuario ? { username: usuario.username, admin: usuario.admin } : null; 
 }
 
 router.get("/logout", (req, res) => {
-  req.session.destroy(); // Saca todas las sesiones
+  req.session.destroy(); //asi libramos todo
   res.clearCookie("access_token"); 
   res.redirect("/");
 });
